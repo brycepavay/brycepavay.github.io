c60e5c260706e90b3205750d9df6e6d0481738fe
Bryce Pavay <pavayb@yahoo.com>
2020-10-29 14:41:05 -0400

    1

--- public/lab_7/script.js
+++ public/lab_7/script.js
@@ -22,6 +22,7 @@ function runThisWithResultsFromServer(jsonFromServer) {
     },
     axisX:{
       interval: 1
+      labelFontSize: 12
     },
     axisY2:{
       interlacedColor: "rgba(1,77,101,.2)",
@@ -34,24 +35,7 @@ function runThisWithResultsFromServer(jsonFromServer) {
       axisYType: "secondary",
       color: "#014D65",
       dataPoints: [
-        { y: 3, label: "Sweden" },
-        { y: 7, label: "Taiwan" },
-        { y: 5, label: "Russia" },
-        { y: 9, label: "Spain" },
-        { y: 7, label: "Brazil" },
-        { y: 7, label: "India" },
-        { y: 9, label: "Italy" },
-        { y: 8, label: "Australia" },
-        { y: 11, label: "Canada" },
-        { y: 15, label: "South Korea" },
-        { y: 12, label: "Netherlands" },
-        { y: 15, label: "Switzerland" },
-        { y: 25, label: "Britain" },
-        { y: 28, label: "Germany" },
-        { y: 29, label: "France" },
-        { y: 52, label: "Japan" },
-        { y: 103, label: "China" },
-        { y: 134, label: "US" }
+ 
       ]
     }]
   });
