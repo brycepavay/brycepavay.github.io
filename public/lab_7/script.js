81f54fdce99f53f068c26fa76895215966946ee0
Bryce Pavay <pavayb@yahoo.com>
2020-10-29 13:59:03 -0400

    Update script.js

--- public/lab_7/script.js
+++ public/lab_7/script.js
@@ -14,6 +14,49 @@ function runThisWithResultsFromServer(jsonFromServer) {
   // Process your restaurants list
   // Make a configuration object for your chart
   // Instantiate your chart
+  const chart = new CanvasJS.Chart("chartContainer", {
+    animationEnabled: true,
+    
+    title:{
+      text:"Places to eat out in future"
+    },
+    axisX:{
+      interval: 1
+    },
+    axisY2:{
+      interlacedColor: "rgba(1,77,101,.2)",
+      gridColor: "rgba(1,77,101,.1)",
+      title: "Number of Companies"
+    },
+    data: [{
+      type: "bar",
+      name: "companies",
+      axisYType: "secondary",
+      color: "#014D65",
+      dataPoints: [
+        { y: 3, label: "Sweden" },
+        { y: 7, label: "Taiwan" },
+        { y: 5, label: "Russia" },
+        { y: 9, label: "Spain" },
+        { y: 7, label: "Brazil" },
+        { y: 7, label: "India" },
+        { y: 9, label: "Italy" },
+        { y: 8, label: "Australia" },
+        { y: 11, label: "Canada" },
+        { y: 15, label: "South Korea" },
+        { y: 12, label: "Netherlands" },
+        { y: 15, label: "Switzerland" },
+        { y: 25, label: "Britain" },
+        { y: 28, label: "Germany" },
+        { y: 29, label: "France" },
+        { y: 52, label: "Japan" },
+        { y: 103, label: "China" },
+        { y: 134, label: "US" }
+      ]
+    }]
+  });
+  chart.render();
+
 }
 
 document.body.addEventListener('submit', async (e) => {
@@ -32,47 +75,4 @@ document.body.addEventListener('submit', async (e) => {
       console.log(err);
     });
 
-
-    const chart = new CanvasJS.Chart("chartContainer", {
-      animationEnabled: true,
-      
-      title:{
-        text:"Places to eat out in future"
-      },
-      axisX:{
-        interval: 1
-      },
-      axisY2:{
-        interlacedColor: "rgba(1,77,101,.2)",
-        gridColor: "rgba(1,77,101,.1)",
-        title: "Number of Companies"
-      },
-      data: [{
-        type: "bar",
-        name: "companies",
-        axisYType: "secondary",
-        color: "#014D65",
-        dataPoints: [
-          { y: 3, label: "Sweden" },
-          { y: 7, label: "Taiwan" },
-          { y: 5, label: "Russia" },
-          { y: 9, label: "Spain" },
-          { y: 7, label: "Brazil" },
-          { y: 7, label: "India" },
-          { y: 9, label: "Italy" },
-          { y: 8, label: "Australia" },
-          { y: 11, label: "Canada" },
-          { y: 15, label: "South Korea" },
-          { y: 12, label: "Netherlands" },
-          { y: 15, label: "Switzerland" },
-          { y: 25, label: "Britain" },
-          { y: 28, label: "Germany" },
-          { y: 29, label: "France" },
-          { y: 52, label: "Japan" },
-          { y: 103, label: "China" },
-          { y: 134, label: "US" }
-        ]
-      }]
-    });
-    chart.render();
 });
